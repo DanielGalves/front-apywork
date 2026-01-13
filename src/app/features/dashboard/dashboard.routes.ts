2ec9701968f0f7533ac97ac3781dac97e6d95993
Daniel Gama Alves <danielgama1@live.com>
2026-01-12 22:55:28 -0300

    feat: Dashboard com recurso de layout base

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardHomeComponent } from './pages/dashboard-home/dashboard-home.component/dashboard-home.component';
 
-export const DASHBOARD_ROUTES: Routes = [];
+export const DASHBOARD_ROUTES: Routes = [
+  {
+    path: '',
+    component: DashboardHomeComponent
+  }
+];
