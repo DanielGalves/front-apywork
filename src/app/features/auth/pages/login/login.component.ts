70d7afdfe1d06c56d5d78490248fb46fbd87d7ae
Daniel Gama Alves <danielgama1@live.com>
2026-01-12 22:33:29 -0300

    feat: pagina de login com recurso auth (standalone)

---
+++ src/app/features/auth/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  form;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  submit(): void {
+    if (this.form.valid) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+}
